f10ca8a10b310ae23d9b1500fc8afcaa4cc55bef
Igor Krysov <igorkrysov@mail.ru>
2017-10-05 22:32:43 +0600

    migration

---
+++ database/migrations/2017_10_05_154726_game_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('session_id')->nullable()->unsigned();
+            $table->foreign('session_id')->references('id')->on('sessions');
+            $table->enum('win', array('pc','user'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
